b08ae13f9cb599ef578bce18b89330ab47c64ec4
kotowaluer <zj1045637489@gmail.com>
2022-07-27 14:31:22 +0800

    fork

--- train.py
+++ train.py
@@ -55,14 +55,15 @@ if __name__ == '__main__':
                 save_result = total_iters % opt.update_html_freq == 0
                 model.compute_visuals()
                 visualizer.display_current_results(model.get_current_visuals(), epoch, save_result)
-
             if total_iters % opt.print_freq == 0:    # print training losses and save logging information to the disk
                 losses = model.get_current_losses()
                 t_comp = (time.time() - iter_start_time) / opt.batch_size
                 visualizer.print_current_losses(epoch, epoch_iter, losses, t_comp, t_data)
-                if opt.display_id > 0:
-                    visualizer.plot_current_losses(epoch, float(epoch_iter) / dataset_size, losses)
-
+                # if opt.display_id > 0:
+                #     try:
+                #         visualizer.plot_current_losses(epoch, float(epoch_iter) / dataset_size, losses)
+                #     except:
+                #         continue
             if total_iters % opt.save_latest_freq == 0:   # cache our latest model every <save_latest_freq> iterations
                 print('saving the latest model (epoch %d, total_iters %d)' % (epoch, total_iters))
                 save_suffix = 'iter_%d' % total_iters if opt.save_by_iter else 'latest'
